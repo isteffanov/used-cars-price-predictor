cda9b8c762aa507307c402ccea467f4544265a54
Ivan Stefanov <isteffanov@gmail.com>
2023-02-04 18:31:18 +0200

    Adds evaluate_module function

--- price_predictor/evaluate.py
+++ price_predictor/evaluate.py
@@ -1,13 +1,21 @@
-import numpy as np
+from pandas import DataFrame
+from sklearn.base import BaseEstimator
 from sklearn.metrics import (
     mean_squared_error,
-    accuracy_score,
     r2_score
 )
 
+def evaluate_model(
+    model: BaseEstimator,
+    X_test: DataFrame,
+    y_test: DataFrame) -> dict:
+    
+    y_pred = model.predict(X_test)
+    return evaluate(y_test, y_pred)
+
 def evaluate(y_true: list, y_pred: list) -> dict:
     return {
-        'accuracy': accuracy_score(y_true, y_pred),
+        # 'accuracy': accuracy_score(y_true, y_pred),
         'mse': mean_squared_error(y_true, y_pred),
         'r2': r2_score(y_true, y_pred)
     }
